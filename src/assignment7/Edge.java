9f39e79263464f8bc3bc9eb6be8807b9dfcf2ebf
Angelique <angelique9514bautista@gmail.com>
2018-04-29 23:02:31 -0500

    added hover effects

--- src/assignment7/Edge.java
+++ src/assignment7/Edge.java
@@ -1,8 +1,12 @@
 package assignment7;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class Edge extends Pane{
@@ -18,12 +22,16 @@ public class Edge extends Pane{
 	
 	protected int weight;
 	Line line;
+	Text label;
 	
 	public Edge(Node source, Node target, int weight) {
 		this.source = source;
 		this.target = target;
 		this.weight = weight;
-				
+		
+		source.addConnectedEdge(target, this);
+		target.addConnectedEdge(source, this);
+		
 		line = new Line();
 		
 		line.startXProperty().bind(source.layoutXProperty().add(source.getBoundsInParent().getWidth() / 2.0));
@@ -38,11 +46,63 @@ public class Edge extends Pane{
 		centerLoc.setX(line.getStartX() + ((line.getEndX() - line.getStartX()) / 2));
 		centerLoc.setY(line.getStartY() + ((line.getEndY() - line.getStartY()) / 2));
 		
-		Text label = new Text(Integer.toString(weight));
+
+		getChildren().add(line);
+		showLabel();
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		result = prime * result + ((target == null) ? 0 : target.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		if (target == null) {
+			if (other.target != null)
+				return false;
+		} else if (!target.equals(other.target))
+			return false;
+		return true;
+	}
+
+	private void showLabel() {
+		label = new Text(Integer.toString(weight));
 		label.relocate(centerLoc.getX() - 10, centerLoc.getY());
 		label.setFill(Color.RED);
-		
-		getChildren().addAll(line, label);
+		getChildren().add(label);
+	}
+	
+	private void hideLabel() {
+		getChildren().remove(label);
+	}
+	
+	public void highlight() {
+		hideLabel();
+		showLabel();
+		label.setFont(Font.font(Font.getDefault().toString(), FontWeight.BOLD, 18));
+	}
+	
+	public void deHighlight() {
+		hideLabel();
+		showLabel();
 	}
 	
 }
--- src/assignment7/Node.java
+++ src/assignment7/Node.java
@@ -1,11 +1,70 @@
 package assignment7;
 
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 public class Node extends StackPane{
+	private ArrayList<Node> connectedNodes = new ArrayList<Node>();
+	private ArrayList<Edge> connectedEdges = new ArrayList<Edge>();
+	
+	public void addConnectedEdge(Node n, Edge e) {
+		boolean nFlag = false;
+		boolean eFlag = false;
+		
+		for(Node node : connectedNodes) {
+			if(node.equals(n)) {
+				nFlag = true;
+			}
+		}
+		
+		for(Edge edge : connectedEdges) {
+			if(edge.equals(n)) {
+				eFlag = true;
+			}
+		}
+		
+		if(!nFlag) {
+			connectedNodes.add(n);
+		}
+		
+		if(!eFlag) {
+			connectedEdges.add(e);
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((doc == null) ? 0 : doc.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (doc == null) {
+			if (other.doc != null)
+				return false;
+		} else if (!doc.equals(other.doc))
+			return false;
+		return true;
+	}
+
 	private final double PI = 3.14;
 	private Document doc;
 	Circle circle;
@@ -14,6 +73,8 @@ public class Node extends StackPane{
 
 	private Coordinates loc = new Coordinates();
 	private int layer = 0;
+	
+	private Text label;
 	
 	private static double halfParentSize;
 	private static int nodeCounter = 0;
@@ -73,10 +134,59 @@ public class Node extends StackPane{
 			rad = 15;
 		}		
 		circle = new Circle(rad, Color.CORNFLOWERBLUE);
+		circle.setStroke(Color.MIDNIGHTBLUE);
 		id = new Text(Integer.toString(this.doc.getId()));
 		
 		getChildren().addAll(circle);
 		relocate(loc.getX(), loc.getY());
+		
+		this.setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				showDoc();
+				for(Node n : connectedNodes) {
+					n.showDoc();
+				}
+				
+				for(Edge e : connectedEdges) {
+					e.highlight();
+				}
+			}
+			
+		});
+		
+		this.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				hideDoc();
+				for(Node n : connectedNodes) {
+					n.hideDoc();
+				}
+				
+				for(Edge e : connectedEdges) {
+					e.deHighlight();
+				}
+			}
+			
+		});
+	}
+	
+	public void showDoc() {
+		label = new Text(this.doc.getName());
+		label.relocate(loc.getX(), loc.getY());
+		label.setFill(Color.WHITE);
+		label.setStroke(Color.BLACK);
+		label.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+		circle.setRadius(rad * 1.25);
+		
+		getChildren().add(label);
+	};
+	
+	public void hideDoc() {
+		getChildren().remove(label);
+		circle.setRadius(rad);
 	}
 	
 	public double getRadius() {
